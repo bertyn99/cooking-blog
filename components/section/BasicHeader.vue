a2d8623eb0d9617f5db7167a29b4d31e45191c84
Bertyn Boulikou <bboulikou@gmail.com>
2023-05-11 18:03:33 +0200

    fix ui remove dark color

--- components/section/BasicHeader.vue
+++ components/section/BasicHeader.vue
@@ -30,7 +30,7 @@ const subHeaderMenu = computed(() => {
 
 <template>
   <header class="w-full h-24 transparent">
-    <nav class="border-gray-200 dark:bg-gray-900">
+    <nav class="border-gray-200">
       <div
         class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
       >
@@ -40,9 +40,8 @@ const subHeaderMenu = computed(() => {
             class="h-8 mr-3"
             alt="Flowbite Logo"
           />
-          <span
-            class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"
-            >Flowbite</span
+          <span class="self-center text-2xl font-semibold whitespace-nowrap"
+            >Journal du Cuistot</span
           >
         </a>
         <div class="flex md:order-2">
@@ -90,7 +89,7 @@ const subHeaderMenu = computed(() => {
             <input
               type="text"
               id="search-navbar"
-              class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
               placeholder="Search..."
             />
           </div>
@@ -147,18 +146,18 @@ const subHeaderMenu = computed(() => {
             <input
               type="text"
               id="search-navbar"
-              class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
               placeholder="Search..."
             />
           </div>
           <ul
-            class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700"
+            class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0"
           >
             <NuxtLink
               v-for="link in links"
               :to="link.url"
-              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              :active-class="'bg-blue-700 text-white'"
+              class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0"
+              :active-class="'bg-blue-700 md:bg-transparent text-white md:text-blue-700'"
               aria-current="page"
               >{{ link.name }}</NuxtLink
             >
