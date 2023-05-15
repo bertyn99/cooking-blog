a528e2aa5702efea491b036af211ed78770bd299
bertyn99 <bboulikou@gmail.com>
2023-05-16 00:08:55 +0200

    feat: add SHare component

---
+++ components/Share.vue
@@ -0,0 +1,53 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="flex flex-wrap divide-y md:divide-x py-1 items-center gap-6">
+    <div class="flex gap-2">
+      <img
+        data-del="avatar"
+        src="https://easymeals.qodeinteractive.com/wp-content/uploads/2020/05/Untitled-2-100x100.png"
+        class="max-w-full h-auto leading-6 text-black align-middle cursor-pointer rounded-full"
+        height="48"
+        width="48"
+      />
+      <div class="flex flex-col">
+        <NuxtLink
+          itemprop="author"
+          class="relative p-0 m-0 text-xs font-semibold tracking-widest leading-6 text-black uppercase align-baseline border-0 cursor-pointer hover:text-stone-500"
+          href="https://easymeals.qodeinteractive.com/author/taylor-kinny/"
+          style="
+            outline: 0px;
+            text-decoration: none;
+            transition: color 0.2s ease-out 0s;
+            background-position: 0px center;
+            top: 2px;
+          "
+        >
+          Taylor Kinny
+        </NuxtLink>
+
+        <datetime
+          itemprop="dateCreated"
+          class="relative p-0 m-0 text-xs leading-6 align-baseline border-0 text-stone-500"
+        >
+          May 8, 2020
+        </datetime>
+      </div>
+    </div>
+    <div class="py-3 md:py-0 flex-1 flex gap-2 items-center px-5">
+      <span>SHARE:</span>
+      <NuxtLink class="text-white bg-blue-500 px-6 py-1">
+        <Icon name="akar-icons:facebook-fill" class="h-5 w-5" />
+      </NuxtLink>
+      <NuxtLink class="text-white bg-sky-400 px-6 py-1">
+        <Icon name="akar-icons:twitter-fill" class="h-5 w-5" />
+      </NuxtLink>
+      <NuxtLink class="text-white bg-pink-500 px-6 py-1">
+        <Icon name="akar-icons:instagram-fill" class="h-5 w-5" />
+      </NuxtLink>
+      <NuxtLink class="text-white bg-red-500 px-6 py-1">
+        <Icon name="akar-icons:pinterest-fill" class="h-5 w-5" />
+      </NuxtLink>
+    </div>
+  </div>
+</template>
