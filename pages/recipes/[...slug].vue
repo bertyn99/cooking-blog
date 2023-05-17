28acad620ef509d3c62d317d99fc6b24b8ab3c52
bertyn99 <bboulikou@gmail.com>
2023-05-17 19:05:34 +0200

    fix responsive info article

--- pages/recipes/[...slug].vue
+++ pages/recipes/[...slug].vue
@@ -6,25 +6,25 @@ definePageMeta({ layout: "content" });
   <SectionHeroArticle>
     <template #info>
       <p
-        class="flex-[0_0_auto] items-center mx-2 h-6 text-xs leading-6 font-semibold tracking-widest text-black uppercase align-baseline border-0"
+        class="items-center mx-2 h-6 text-xs leading-6 font-semibold tracking-widest text-black uppercase align-baseline border-0"
       >
         <Icon name="ic:sharp-access-time" class="h-3 w-3 text-gray-500" />
         30 minutes
       </p>
       <p
-        class="flex-[0_0_auto] items-center mx-2 h-6 text-xs leading-6 font-semibold tracking-widest text-black uppercase align-baseline border-0"
+        class="items-center mx-2 h-6 text-xs leading-6 font-semibold tracking-widest text-black uppercase align-baseline border-0"
       >
         <Icon name="icon-park-outline:good-two" class="h-3 w-3 text-gray-500" />
         super easy
       </p>
       <p
-        class="flex-[0_0_auto] mx-2 h-6 text-xs leading-6 font-semibold tracking-widest text-black uppercase align-baseline border-0"
+        class="mx-2 h-6 text-xs leading-6 font-semibold tracking-widest text-black uppercase align-baseline border-0"
       >
         <Icon name="mdi:silverware-fork-knife" class="h-3 w-3 text-gray-500" />
         serves 1
       </p>
       <div
-        class="flex-[0_0_auto] p-0 my-0 mx-2 h-6 text-xs font-semibold tracking-widest text-black uppercase align-baseline border-0"
+        class="p-0 my-0 mx-2 h-6 text-xs font-semibold tracking-widest text-black uppercase align-baseline border-0"
       >
         <a
           itemprop="url"
