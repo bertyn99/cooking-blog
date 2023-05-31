8ab7a806cf305a319b9cdd9d61f3d1bcfc193b72
bertyn99 <bboulikou@gmail.com>
2023-05-31 17:45:44 +0200

    add strapi type

--- types/strapiMeta.d.ts
+++ types/strapiMeta.d.ts
@@ -1,5 +1,5 @@
-type Cover = {};
-type Category = {
+export type Cover = {};
+export type Category = {
   data?: {
     id?: number;
     attributes?: {
@@ -362,7 +362,7 @@ export type Recipe = {
     };
   }[];
 };
-type Article = {
+export type Article = {
   id?: number;
   attributes?: {
     content?: string;
