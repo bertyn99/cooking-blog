e4bdaf14ac0b8b726fb5590b0492bb0bdc1b9de2
bertyn99 <bboulikou@gmail.com>
2023-06-02 09:35:12 +0200

    add page list blog wip

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,40 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Recipe } from "~/types/strapiMeta";
 
-<template></template>
+const { find } = useStrapi();
+
+const { data: articles } = await useAsyncData<Recipe>(`articles`, () =>
+  find(`articles?populate=*`)
+);
+console.log(articles);
+
+const { data: categories } = await useAsyncData(`categories`, () =>
+  find(`categories?fields=name`)
+);
+
+const formatCategories = computed(() =>
+  categories.value?.data.map((category) => {
+    return { name: category.attributes?.name, id: category.id };
+  })
+);
+
+const checkedCategories = ref([]);
+console.log(formatCategories);
+</script>
+
+<template>
+  <div class="py-24 mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <h1 class="text-4xl font-bold tracking-tight">Recettes</h1>
+  </div>
+  <section
+    aria-labelledby="products-heading"
+    class="mx-auto max-w-7xl px-4 sm:px-6"
+  >
+    <div class="pb-24 pt-6">
+      <Filter :categories="formatCategories" :value="checkedCategories" />
+      <div class="lg:col-span-3">
+        <ArticleList :articles="articles.data"></ArticleList>
+      </div>
+    </div>
+  </section>
+</template>
