4307fdcafea496385993bfec7901d75568d46a7f
bertyn99 <bboulikou@gmail.com>
2023-07-09 11:05:14 +0200

    fix sme error 404 and 500

--- middleware/brokenLinksRedirect.ts
+++ middleware/brokenLinksRedirect.ts
@@ -1,8 +1,34 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const listRedirects = [
     { from: "/techniques-de-cuisine", to: "/bases-culinaires" },
+    {
+      from: "/recipe/tout-sur-le-the-indien",
+      to: "/blog/tout-sur-le-the-indien",
+    },
+    {
+      from: "/articles/tout-sur-le-the-indien",
+      to: "/blog/tout-sur-le-the-indien",
+    },
+    {
+      from: "/articles/le-roi-du-riz-tout-ceque-vous-devez-savoir-sur-le-riz-basmati",
+      to: "/blog/le-roi-du-riz-tout-ceque-vous-devez-savoir-sur-le-riz-basmati",
+    },
+    {
+      from: "/articles/les-legumes-de-base-de-la-cuisine-asiatique",
+      to: "/blog/les-legumes-de-base-de-la-cuisine-asiatique",
+    },
+    {
+      from: "/recette/les-sauces-indispensables-pour-la-cuisine-asiatique",
+      to: "/blog/les-sauces-indispensables-pour-la-cuisine-asiatique",
+    },
+
+    {
+      from: "/recette/le-roi-du-riz-tout-ceque-vous-devez-savoir-sur-le-riz-basmati",
+      to: "/blog/le-roi-du-riz-tout-ceque-vous-devez-savoir-sur-le-riz-basmati",
+    },
   ];
-  let redirectPath = listRedirects.filter((v) => v.from == to.path);
+  //
+  https: let redirectPath = listRedirects.filter((v) => v.from == to.path);
 
   if (redirectPath.length !== 0 && redirectPath[0].to) {
     return navigateTo(redirectPath[0].to, { redirectCode: 301 });
