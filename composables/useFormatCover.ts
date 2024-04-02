ac39b75789a3984654ba924d2dadb698e7de63e2
Bertyn Boulikou <bboulikou@gmail.com>
2024-04-02 15:42:43 +0200

    Fix format URL in useFormatUrlCover function

--- composables/useFormatCover.ts
+++ composables/useFormatCover.ts
@@ -3,13 +3,13 @@ import { Cover, sizeImg } from "~/types/strapiMeta";
 export const useFormatUrlCover = (cover: Cover, size?: string) => {
   const config = useRuntimeConfig();
 
-  const format = cover.data?.attributes?.formats[size] || null;
+  const format = cover.data?.attributes?.formats && size ? cover.data.attributes.formats[size] : null;
 
-  if (typeof format !== "undefined" || format !== null) {
-    const url = format?.url;
+  if (typeof format !== "undefined" && format !== null) {
+    const url = format.url;
     return url
       ? url
-      : `${cover?.data.attributes?.hash}${cover?.data.attributes?.ext}`;
+      : `${cover?.data?.attributes?.hash}${cover?.data?.attributes?.ext}`;
   }
-  return `${cover?.data.attributes?.hash}${cover?.data.attributes?.ext}`;
+  return `${cover?.data?.attributes?.hash}${cover?.data?.attributes?.ext}`;
 };
