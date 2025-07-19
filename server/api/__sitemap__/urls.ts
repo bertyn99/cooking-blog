fff27a6ac65ca58cde83751ced0511936ae5b386
bertyn99 <bboulikou@gmail.com>
2025-07-19 16:48:55 +0200

    refacto: restructure the sitemap and divide it in sub sitemap

--- server/api/__sitemap__/urls.ts
+++ server/api/__sitemap__/urls.ts
@@ -2,76 +2,79 @@ import { defineSitemapEventHandler } from '#imports'
 import { generateSlug } from "~/utils/format";
 
 export default defineSitemapEventHandler(async () => {
-    // Fetch all documents in parallel
-    const [pagesResponse, articlesResponse, recipesResponse] = await Promise.all([
-        $fetch("https://admin.journalducuistot.fr/api/pages?populate=parent"),
-        $fetch("https://admin.journalducuistot.fr/api/articles?pagination[pageSize]=100"),
-        $fetch("https://admin.journalducuistot.fr/api/recipes?pagination[pageSize]=100")
-    ]);
+  // Fetch all documents in parallel
+  const [pagesResponse, articlesResponse, recipesResponse] = await Promise.all([
+    $fetch("https://admin.journalducuistot.fr/api/pages?populate=parent"),
+    $fetch("https://admin.journalducuistot.fr/api/articles?pagination[pageSize]=100"),
+    $fetch("https://admin.journalducuistot.fr/api/recipes?pagination[pageSize]=100")
+  ]);
 
-    const pages = pagesResponse.data;
-    const articles = articlesResponse.data;
-    const recipes = recipesResponse.data;
+  const pages = pagesResponse.data;
+  const articles = articlesResponse.data;
+  const recipes = recipesResponse.data;
 
-    const urls = [];
+  const urls = [];
 
-    // Add homepage
+  // Add homepage
+  urls.push({
+    loc: "/",
+    changefreq: "daily",
+    priority: 1,
+  });
+
+  // Add pages
+  for (const doc of pages) {
     urls.push({
-        loc: "/",
-        changefreq: "daily",
-        priority: 1,
+      loc: generateSlug(doc.slug, doc.parent),
+      lastmod: doc.updatedAt,
+      priority: 0.8,
+      changefreq: "daily",
+      _sitemap: 'pages',
+      /* img: [
+        {
+          url: doc.image,
+          caption: doc.description,
+          title: doc.title,
+        },
+      ], */
     });
+  }
 
-    // Add pages
-    for (const doc of pages) {
-        urls.push({
-            loc: generateSlug(doc.attributes.slug, doc.attributes.parent),
-            lastmod: doc.attributes.updatedAt,
-            priority: 0.8,
-            changefreq: "daily",
-            /* img: [
-              {
-                url: doc.image,
-                caption: doc.description,
-                title: doc.title,
-              },
-            ], */
-        });
-    }
+  // Add articles
+  for (const doc of articles) {
+    urls.push({
+      loc: `/blog/${doc.slug}`,
+      lastmod: doc.updatedAt,
+      priority: 0.6,
+      changefreq: "daily",
+      _sitemap: 'blog',
+      /* img: [
+        {
+          url: doc.image,
+          caption: doc.description,
+          title: doc.title,
+        },
+      ], */
+    });
+  }
 
-    // Add articles
-    for (const doc of articles) {
-        urls.push({
-            loc: `/blog/${doc.attributes.slug}`,
-            lastmod: doc.attributes.updatedAt,
-            priority: 0.6,
-            changefreq: "daily",
-            /* img: [
-              {
-                url: doc.image,
-                caption: doc.description,
-                title: doc.title,
-              },
-            ], */
-        });
-    }
+  // Add recipes
+  for (const doc of recipes) {
+    urls.push({
+      loc: `/recette/${doc.slug}`,
+      lastmod: doc.updatedAt,
+      priority: 0.7,
+      changefreq: "daily",
+      _sitemap: 'recette',
+      /* img: [
+        {
+          url: doc.image,
+          caption: doc.description,
+          title: doc.title,
+        },
+      ], */
+    });
+  }
 
-    // Add recipes
-    for (const doc of recipes) {
-        urls.push({
-            loc: `/recette/${doc.attributes.slug}`,
-            lastmod: doc.attributes.updatedAt,
-            priority: 0.7,
-            changefreq: "daily",
-            /* img: [
-              {
-                url: doc.image,
-                caption: doc.description,
-                title: doc.title,
-              },
-            ], */
-        });
-    }
-
-    return urls;
+  return urls;
 });
